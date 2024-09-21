c0a74edb9fbe8a2a6d7823103ba2490798efbd24
Murillo Ferraz <marteldelfer@gmail.com>
2024-09-20 22:22:26 -0300

    Create Spring project

---
+++ chat/src/main/java/com/martel/chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package com.martel.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
